f8d69be50a80fcb36c7b70194dca835739a17ce8
jdxxmahmud <junaidmahmud@iut-dhaka.edu>
2023-08-14 23:23:04 +0600

    Implementing security (jwt)

--- backend/restaurant_backend/src/main/java/com/project/restaurant/entity/User.java
+++ backend/restaurant_backend/src/main/java/com/project/restaurant/entity/User.java
@@ -1,32 +1,40 @@
 package com.project.restaurant.entity;
 
+import com.project.restaurant.util.enums.Role;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.sql.Timestamp;
+import java.util.Collection;
+import java.util.List;
 
 @Table(name = "USERS")
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class User{
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID")
     private Long id;
     @Column(name = "ROLE_ID")
     private Long roleId;
+    @Column(name = "EMAIL")
+    private String email;
+    @Column(name = "PASSWORD")
+    private String password;
     @Column(name = "NAME")
     private String name;
     @Column(name = "PHONE_NUMBER")
     private String phoneNumber;
     @Column(name = "ADDRESS")
     private String address;
-    @Column(name = "EMAIL")
-    private String email;
     @Column(name = "CREATED_AT")
     private Timestamp createdAt;
     @Column(name = "UPDATED_AT")
@@ -35,4 +43,43 @@ public class User{
     private Long createdBy;
     @Column(name = "UPDATED_BY")
     private Long updatedBy;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "ROLE")
+    private Role role;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
