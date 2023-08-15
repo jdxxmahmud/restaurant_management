1f2eac1c72d25cdfc29a080a2137a8dfb7762e83
jdxxmahmud <junaidmahmud@iut-dhaka.edu>
2023-08-15 14:32:25 +0600

    uPDATE

--- backend/restaurant_backend/src/main/java/com/project/restaurant/respository/FoodCategoryRepository.java
+++ backend/restaurant_backend/src/main/java/com/project/restaurant/respository/FoodCategoryRepository.java
@@ -2,7 +2,9 @@ package com.project.restaurant.respository;
 
 import com.project.restaurant.entity.FoodCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FoodCategoryRepository extends JpaRepository<FoodCategory, Long>
 {
 
